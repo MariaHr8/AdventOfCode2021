88e7a13f4f5318c6f109a4133d656d782608db19
Maria Hristova <fcmj@abv.bg>
2021-12-01 22:33:50 +0000

    Completed Part Two of Day One

--- Day1/checkPartTwo.js
+++ Day1/checkPartTwo.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 let input_buffer = [];
 let queue = [];
-let measurement = 0;
+let three_measurement = 0;
 
 setup =  () => {
     let dataFile = fs.createReadStream(`submarine.data`, { encoding: 'utf8', fd: null });
@@ -22,27 +22,35 @@ setup =  () => {
     });
 
     dataFile.on('end', function () {
-        console.log(`There are ${measurement} measurements that are larger than the previous measurement.`)
+        console.log(`There are ${three_measurement} measurements that are larger than the previous measurement.`)
         console.log("Done");
     });
 }
 
 checkQueue = (word, array) => {
-    if(array.length < 1){
+    if(array.length < 3){
         array.push(word);
         console.log(word + " (N/A - no previous measurement)");
-    }else{
+    }
+    else{
+        let oldSum = 0, sum =0;
+        oldSum = array[0] + array[1] + array[2];
         array.push(word);
-        console.log(word);
-        if(array[0] <= array[1]){
-            console.log("increased");
-            measurement++;
-        }else if (array[0] >= array[1]){
+        sum = array[1] + array[2] + array[3];
+        console.log("NewSum - "+ sum);
+        if(oldSum > sum){
             console.log("decreased");
+        }else if (sum> oldSum){
+            console.log("increased");
+            three_measurement++;
         }else{
             console.log("no change")
         }
+
+
+
         array.shift();
+
     }
 }
 
